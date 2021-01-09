9479e69396fb16e9da85758086da89645841d221
Exequiel Ignacio SOsa <75383904+exequielsosa@users.noreply.github.com>
2021-01-09 13:17:30 -0300

    Proyecto Final App Tienda

---
+++ src/componentes/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error404 = () => {
+    return ( 
+        <div>
+            <h1>ERROR 404</h1>
+            <p>La pagina que buscas no se encuentra o fue removida</p>
+            <p>¡Suerte en la Proxima!</p>
+        </div>
+     );
+}
+ 
+export default Error404;
---
+++ src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend elementum risus, non consectetur magna posuere sit amet. Maecenas dapibus semper est quis ultricies. Ut nec ipsum in erat tincidunt tempor. Sed ante nibh, gravida ut orci at, elementum lacinia nibh. Curabitur quis ornare diam, id faucibus tortor. Nullam interdum ligula congue, tincidunt ligula in, rutrum erat. Aenean turpis metus, faucibus ac maximus at, tempor in est. Mauris consectetur varius dui, ut rutrum nisi dapibus vitae. Etiam vitae diam id orci varius pretium at vel metus. Vivamus venenatis ipsum nec diam facilisis rhoncus. Sed vel felis et quam accumsan dignissim. Nulla facilisi.</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
